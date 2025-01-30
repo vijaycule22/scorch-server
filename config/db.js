eb6af0c100c3c1a7b410deeb35dee5ec21b1daa7
vavula <vavula@ghx.com>
2025-01-30 17:07:51 +0530

    added postgress sslmode

--- config/db.js
+++ config/db.js
@@ -28,7 +28,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL  + "?sslmode=require",
   ssl: {
     rejectUnauthorized: false,
   },
